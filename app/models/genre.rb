3c5bb526998e95a0265595b6e94928a8f9bb11de
Allan Farca <allan.farca@gmail.com>
2020-06-11 03:17:21 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -12,7 +12,7 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    Song.joins(:genres)
+    Genre.joins(:songs)
     self.songs
   end
 end
